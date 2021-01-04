554d4d727a8c6e3753271242540dc48f4f744d44
Adrián Lizaga <759090@unizar.es>
2021-01-04 22:50:14 +0100

    cambio en el enum USERID_IN_USE para soportar el cambio de MUSIC

--- src/main/java/domain/enums/ScreenIndicator.java
+++ src/main/java/domain/enums/ScreenIndicator.java
@@ -5,7 +5,7 @@ public enum ScreenIndicator {
   MUSIC_LOGIN_WINDOW("sign on"),
   MUSIC_USERID_UNAUTHORIZED("Userid is not authorized"),
   MUSIC_PWD_INCORRECT("Password incorrect"),
-  MUSIC_USERID_IN_USE("Userid in use"),
+  MUSIC_USERID_IN_USE("Userid is in use"),
   MUSIC_COMMAND_LINE("Full Screen Interface for MUSIC"),
   TASKS2_MAIN_WINDOW("TASK MANAGEMENT 2.0 BY TURO-SL SOFT"),
   TASKS2_NEW_TASK_FILE_WINDOW("**NEW TASK FILE**"),
